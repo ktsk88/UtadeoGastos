3856e44e154ca5c561c1e7fe5fb714384000d6c3
Sinthia Katherine Rodriguez Valentin <sinthia.rodriguez@grantitan.com>
2024-11-11 19:59:26 -0500

    .

---
+++ Ninfa.Common/Constant/Promps.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ninfa.Common
+{
+    public class Promps
+    {
+        public const string FindIntention = "Puedes detectar la intencion del usuario que envia el siguiente mensaje \"{0}\" "; //mensaje
+        public const string UserDude = "Un usuario llamado {0} con cedula terminada en {1} pregunta \"{2}\""; //Nombre- cedula - pregunta
+        public const string UserEasyDude = "Un usuario llamado {0} pregunta \"{1}\""; //Nombre -pregunta
+        public const string EasyDude = "Un usuario pregunta \"{0}\""; //Pregunta
+        public const string UserDataDude = "De acuerdo a la siguiente informacion, responder a la pregunta que realiza {0}, \n \"{1}\"  \n {2}"; //Nombre- pregunta - datos
+        public const string RgxGreet = @"(Saludar)";
+        public const string RgxKnowMore = @"(Saber mas)";
+        public const string RgxHowToWork = @"(Como funciona)";
+        public const string RgxConsult = @"(Consulta)";
+        public const string RgxAvailableBalance = @"(Validacion saldo disponible)";
+        public const string RgxInfoCredits = @"(Consulta cuotas credito)";
+        public const string RgxSettings = @"(Configuracion)";
+        public const string RgxInfoYear = @"(Informe año )(\-|\+)(\d{1,2})";
+        public const string RgxInfoMonth = @"(Informe mes )(\-|\+)(\d{1,2})";
+    }
+}
