0aa07bebb72a3496eaca8964ba0255f82ed4136f
Sinthia Katherine Rodriguez Valentin <sinthia.rodriguez@grantitan.com>
2024-11-11 20:00:42 -0500

    .-

---
+++ Ninfa.Integration/GptCommunication.cs
@@ -0,0 +1,18 @@
+﻿using Ninfa.Interface;
+
+using OpenAI.Chat;
+
+namespace Ninfa.Integration
+{
+    public class GptCommunication : IGptCommunication
+    {
+        async Task<string> IGptCommunication.GetIntention(string userMessage)
+        {
+            ChatClient client = new(model: "ft:gpt-4o-mini", apiKey: "sk");
+
+            ChatCompletion chatCompletion = await client.CompleteChatAsync(userMessage);
+
+            return chatCompletion.Content.ToString();
+        }
+    }
+}
